1df14396aa1e265eba7b71789ae8c6922ed039ba
Nicklaus Park <nicklaus.park@gmail.com>
2019-06-17 22:21:32 -1000

    Updates

---
+++ precache-manifest.3239b7a8532e42cfa02dbb2f8fbd0ef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16104a4e93d7425c14c82618e31aa071",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "1fc7e4cdb5aa0cc0dc36",
+    "url": "/react-pokedex/static/css/main.31d0cb87.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/react-pokedex/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "1fc7e4cdb5aa0cc0dc36",
+    "url": "/react-pokedex/static/js/main.fb36fcc9.chunk.js"
+  },
+  {
+    "revision": "49325ecd73a50f0ca547",
+    "url": "/react-pokedex/static/js/runtime~main.0848e284.js"
+  }
+]);
